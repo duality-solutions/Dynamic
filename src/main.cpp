52ca64600bd2c8a87f5ea0105d248c695174985d
Spencer Lievens <spencer.lievens@sterlingcoin.org.uk>
2016-12-02 15:12:49 +0100

    Refactor IsBlockValueValid * refactor IsBlockValueValid to return actual error string, use it in error message for bad-cb-amount
    * make error messages in IsBlockValueValid even more verbose

--- src/main.cpp
+++ src/main.cpp
@@ -2691,7 +2691,7 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
     int64_t nTime3 = GetTimeMicros(); nTimeConnect += nTime3 - nTime2;
     LogPrint("bench", "      - Connect %u transactions: %.2fms (%.3fms/tx, %.3fms/txin) [%.2fs]\n", (unsigned)block.vtx.size(), 0.001 * (nTime3 - nTime2), 0.001 * (nTime3 - nTime2) / block.vtx.size(), nInputs <= 1 ? 0 : 0.001 * (nTime3 - nTime2) / (nInputs-1), nTimeConnect * 0.000001);
 
-    // DARKSILK : MODIFYED TO CHECK STORMNODE PAYMENTS AND SUPERBLOCKS
+    // DARKSILK : MODIFIED TO CHECK STORMNODE PAYMENTS AND SUPERBLOCKS
    bool fStormnodePaid = false;
 
     if(chainActive.Height() > Params().StartStormnodePayments()) {
@@ -2702,12 +2702,10 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
     }
 
     CAmount nExpectedBlockValue = GetStormnodePayment(fStormnodePaid) + GetPoWBlockPayment(pindex->pprev->nHeight, nFees);
-   
-    if(!IsBlockValueValid(block, pindex->nHeight, nExpectedBlockValue)){
-        return state.DoS(100,
-                         error("ConnectBlock() : coinbase pays too much (actual=%d vs limit=%d)",
-                               block.vtx[0].GetValueOut(), nExpectedBlockValue,
-                               REJECT_INVALID, "bad-cb-amount"));
+    std::string strError = "";
+
+    if(!IsBlockValueValid(block, pindex->nHeight, nExpectedBlockValue, strError)){
+        return state.DoS(100, error("ConnectBlock(): %s", strError), REJECT_INVALID, "bad-cb-amount");
     }
 
     if (!IsBlockPayeeValid(block.vtx[0], pindex->nHeight, nExpectedBlockValue)) {
--- src/stormnode-payments.h
+++ src/stormnode-payments.h
@@ -34,7 +34,7 @@ extern CCriticalSection cs_mapStormnodePayeeVotes;
 extern CStormnodePayments snpayments;
 
 /// TODO: all 4 functions do not belong here really, they should be refactored/moved somewhere (main.cpp ?)
-bool IsBlockValueValid(const CBlock& block, int nBlockHeight, CAmount blockReward);
+bool IsBlockValueValid(const CBlock& block, int nBlockHeight, CAmount blockReward, std::string &strErrorRet);
 bool IsBlockPayeeValid(const CTransaction& txNew, int nBlockHeight, CAmount blockReward);
 void FillBlockPayments(CMutableTransaction& txNew, int nBlockHeight, CAmount blockReward, CTxOut& txoutStormnodeRet, std::vector<CTxOut>& voutSuperblockRet);
 std::string GetRequiredPaymentsString(int nBlockHeight);
