1ab3d84ddb5ffb742a756012cd80a0cc449d1f0c
Spencer Lievens <spencer.lievens@sterlingcoin.org.uk>
2017-02-05 21:56:02 +0100

    fix for stormnode.conf on windows

--- src/stormnodeconfig.cpp
+++ src/stormnodeconfig.cpp
@@ -3,10 +3,9 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "net.h"
+#include "netbase.h"
 #include "stormnodeconfig.h"
 #include "util.h"
-#include "ui_interface.h"
 #include "chainparams.h"
 
 #include <boost/filesystem.hpp>
@@ -15,7 +14,7 @@
 CStormnodeConfig stormnodeConfig;
 
 void CStormnodeConfig::add(std::string alias, std::string ip, std::string privKey, std::string txHash, std::string outputIndex) {
-    CStormnodeEntry cme(alias, ip, privKey, txHash, outputIndex);
+    CMasternodeEntry cme(alias, ip, privKey, txHash, outputIndex);
     entries.push_back(cme);
 }
 
@@ -29,7 +28,7 @@ bool CStormnodeConfig::read(std::string& strErr) {
         if (configFile != NULL) {
             std::string strHeader = "# Stormnode config file\n"
                           "# Format: alias IP:port stormnodeprivkey collateral_output_txid collateral_output_index\n"
-                          "# Example: sn1 127.0.0.2:31600 93HaYBVUCYjEMeeH1Y4sBGLALQZE1Yc1K64xiqgX37tGBDQL8Xg 2bcd3c84c84f87eaa86e4e56834c92927a07f9e18718810b92e0d0324456a67c 0\n";
+                          "# Example: mn1 127.0.0.2:19999 93HaYBVUCYjEMeeH1Y4sBGLALQZE1Yc1K64xiqgX37tGBDQL8Xg 2bcd3c84c84f87eaa86e4e56834c92927a07f9e18718810b92e0d0324456a67c 0\n";
             fwrite(strHeader.c_str(), std::strlen(strHeader.c_str()), 1, configFile);
             fclose(configFile);
         }
@@ -59,6 +58,34 @@ bool CStormnodeConfig::read(std::string& strErr) {
                 return false;
             }
         }
+
+        int port = 0;
+        std::string hostname = "";
+        SplitHostPort(ip, port, hostname);
+        if(port == 0 || hostname == "") {
+            strErr = _("Failed to parse host:port string") + "\n"+
+                    strprintf(_("Line: %d"), linenumber) + "\n\"" + line + "\"";
+            streamConfig.close();
+            return false;
+        }
+        int mainnetDefaultPort = Params(CBaseChainParams::MAIN).GetDefaultPort();
+        if(Params().NetworkIDString() == CBaseChainParams::MAIN) {
+            if(port != mainnetDefaultPort) {
+                strErr = _("Invalid port detected in stormnode.conf") + "\n" +
+                        strprintf(_("Port: %d"), port) + "\n" +
+                        strprintf(_("Line: %d"), linenumber) + "\n\"" + line + "\"" + "\n" +
+                        strprintf(_("(must be %d for mainnet)"), mainnetDefaultPort);
+                streamConfig.close();
+                return false;
+            }
+        } else if(port == mainnetDefaultPort) {
+            strErr = _("Invalid port detected in stormnode.conf") + "\n" +
+                    strprintf(_("Line: %d"), linenumber) + "\n\"" + line + "\"" + "\n" +
+                    strprintf(_("(%d could be used only on mainnet)"), mainnetDefaultPort);
+            streamConfig.close();
+            return false;
+        }
+
 
         add(alias, ip, privKey, txHash, outputIndex);
     }
