1e82025d555328cc316dfe4c7767d12737fa6b72
Spencer Lievens <spencer.lievens@sterlingcoin.org.uk>
2017-03-13 15:04:15 +0100

    Change to test mining memory performance

--- src/config/dynamic-config.h
+++ src/config/dynamic-config.h
@@ -18,7 +18,7 @@
 #define CLIENT_VERSION_MAJOR 1
 
 /* Minor version */
-#define CLIENT_VERSION_MINOR 0
+#define CLIENT_VERSION_MINOR 1
 
 /* Build revision */
 #define CLIENT_VERSION_REVISION 0
@@ -308,7 +308,7 @@
 #define PACKAGE_NAME "Dynamic"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Dynamic 1.0.0"
+#define PACKAGE_STRING "Dynamic 1.1.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "dynamic"
@@ -317,7 +317,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0.0"
+#define PACKAGE_VERSION "1.1.0"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
