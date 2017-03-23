e40bdb07346a5be58a7f7440da7854814895d4b2
Spencer Lievens <spencer.lievens@sterlingcoin.org.uk>
2017-03-23 10:18:11 +0100

    HD Wallet Code Improvements

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -2382,7 +2382,8 @@ UniValue getwalletinfo(const UniValue& params, bool fHelp)
             "  \"keys_left\": xxxx,          (numeric) how many new keys are left since last automatic backup\n"
             "  \"unlocked_until\": ttt,      (numeric) the timestamp in seconds since epoch (midnight Jan 1 1970 GMT) that the wallet is unlocked for transfers, or 0 if the wallet is locked\n"
             "  \"paytxfee\": x.xxxx,         (numeric) the transaction fee configuration, set in " + CURRENCY_UNIT + "/kB\n"
-            "  \"masterkeyid\": \"<hash160>\", (string) the Hash160 of the HD master pubkey\n"
+            "  \"hdmasterkeyid\": \"<hash160>\", (string) the Hash160 of the HD master pubkey\n"
+            "  \"hdchildkeyindex\": xxxx,    (numeric) current childkey index\n"
             "}\n"
             "\nExamples:\n"
             + HelpExampleCli("getwalletinfo", "")
@@ -2403,9 +2404,12 @@ UniValue getwalletinfo(const UniValue& params, bool fHelp)
     if (pwalletMain->IsCrypted())
         obj.push_back(Pair("unlocked_until", nWalletUnlockTime));
     obj.push_back(Pair("paytxfee",      ValueFromAmount(payTxFee.GetFeePerK())));
-    CKeyID masterKeyID = pwalletMain->GetHDChain().masterKeyID;
-    if (!masterKeyID.IsNull())
-         obj.push_back(Pair("masterkeyid", masterKeyID.GetHex()));
+    CHDChain hdChain = pwalletMain->GetHDChain();
+    CKeyID masterKeyID = hdChain.masterKeyID;
+    if (!masterKeyID.IsNull()) {
+         obj.push_back(Pair("hdmasterkeyid", masterKeyID.GetHex()));
+         obj.push_back(Pair("hdchildkeyindex", (int64_t)hdChain.nExternalChainCounter));
+     }
     return obj;
 }
 
