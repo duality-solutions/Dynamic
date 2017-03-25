6347f6539b7b5dc0da972e82b6f9be3ceca5b833
Spencer Lievens <spencer.lievens@sterlingcoin.org.uk>
2017-03-25 13:10:38 +0100

    Ensure  is in valid range - If the -maxsigcachesize parameter is set to zero, setup a minimum sized sigcache (2 elements) rather than segfaulting. - Handle maxsigcachesize being negative - Handle maxsigcachesize being too large

--- src/script/sigcache.cpp
+++ src/script/sigcache.cpp
@@ -70,8 +70,9 @@ public:
 
     void Set(const uint256& entry)
     {
-        size_t nMaxCacheSize = GetArg("-maxsigcachesize", DEFAULT_MAX_SIG_CACHE_SIZE) * ((size_t) 1 << 20);
-        if (nMaxCacheSize <= 0) return;
+    // nMaxCacheSize is unsigned. If -maxsigcachesize is set to zero,
+    // setup_bytes creates the minimum possible cache (2 elements).
+    size_t nMaxCacheSize = std::min(std::max((int64_t)0, GetArg("-maxsigcachesize", DEFAULT_MAX_SIG_CACHE_SIZE)), MAX_MAX_SIG_CACHE_SIZE) * ((size_t) 1 << 20);
 
         boost::unique_lock<boost::shared_mutex> lock(cs_sigcache);
         while (memusage::DynamicUsage(setValid) > nMaxCacheSize)
