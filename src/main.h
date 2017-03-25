e5b0728bf496382be2a9dc6781730ae5a41ff636
Spencer Lievens <spencer.lievens@sterlingcoin.org.uk>
2017-03-25 22:13:13 +0100

    Evict orphans which are included or precluded by accepte… …d blocks.
    54326a6 Increase maximum orphan size to 100,000 bytes. (Gregory Maxwell)
    8c99d1b Treat orphans as implicit inv for parents, discard when parents rejected. (Gregory Maxwell)
    11cc143 Adds an expiration time for orphan tx. (Gregory Maxwell)
    db0ffe8 This eliminates the primary leak that causes the orphan map to  always grow to its maximum size. (Gregory Maxwell)
    1b0bcc5 Track orphan by prev COutPoint rather than prev hash (Pieter Wuille)

--- src/main.h
+++ src/main.h
@@ -59,7 +59,11 @@ static const unsigned int DEFAULT_MIN_RELAY_TX_FEE = 10000; // was 1000
 //! Discourage users to set fees higher than this amount (in satoshis) per kB
 static const CAmount HIGH_TX_FEE_PER_KB = 0.01 * COIN;
 /** Default for -maxorphantx, maximum number of orphan transactions kept in memory */
-static const unsigned int DEFAULT_MAX_ORPHAN_TRANSACTIONS = 400;
+static const unsigned int DEFAULT_MAX_ORPHAN_TRANSACTIONS = 100;
+/** Expiration time for orphan transactions in seconds */
+static const int64_t ORPHAN_TX_EXPIRE_TIME = 20 * 60;
+/** Minimum time between orphan transactions expire time checks in seconds */
+static const int64_t ORPHAN_TX_EXPIRE_INTERVAL = 5 * 60;
 /** Default for -limitancestorcount, max number of in-mempool ancestors */
 static const unsigned int DEFAULT_ANCESTOR_LIMIT = 100;
 /** Default for -limitancestorsize, maximum kilobytes of tx + all in-mempool ancestors */
