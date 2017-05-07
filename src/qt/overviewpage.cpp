2346e03c6a6c655ea5b7ab5220a0bcc004d896a2
Spencer Lievens <spencerlievens@users.noreply.github.com>
2017-05-08 00:04:00 +0200

    Update overviewpage.cpp

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -32,8 +32,8 @@
 
 #define ICON_OFFSET 16
 #define DECORATION_SIZE 54
-#define NUM_ITEMS 5
-#define NUM_ITEMS_ADV 7
+#define NUM_ITEMS 7
+#define NUM_ITEMS_ADV 9
 
 class TxViewDelegate : public QAbstractItemDelegate
 {
