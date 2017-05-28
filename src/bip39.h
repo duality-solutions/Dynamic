a4a0e61662136d54c020b2743cc9fbbac289a477
spencerlievens <spencer.lievens@sterlingcoin.org.uk>
2017-05-28 11:44:59 +0200

    SecureVector

--- src/bip39.h
+++ src/bip39.h
@@ -30,10 +30,10 @@ class CMnemonic
 {
 public:
     static SecureString Generate(int strength);    // strength in bits
-    static SecureString FromData(const uint8_t *data, int len);
-    static int Check(SecureString mnemonic);
+    static SecureString FromData(const SecureVector& data, int len);
+    static bool Check(SecureString mnemonic);
     // passphrase must be at most 256 characters or code may crash
     static void ToSeed(SecureString mnemonic, SecureString passphrase, SecureVector& seedRet);
 };
 
-#endif // DYNAMIC_BIP39_H
+#endif
