4119c6096f82e8477cd5d21cc3df641b8c583869
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2017-09-25 13:16:51 +0200

    bitcoin#11335: Replace save|restoreWindowGeometry with Qt functions

--- src/qt/dynamicgui.cpp
+++ src/qt/dynamicgui.cpp
@@ -137,7 +137,11 @@ DynamicGUI::DynamicGUI(const PlatformStyle* _platformStyle, const NetworkStyle* 
     /* Open CSS when configured */
     this->setStyleSheet(GUIUtil::loadStyleSheet());
 
-    GUIUtil::restoreWindowGeometry("nWindow", QSize(850, 550), this);
+    QSettings settings;
+    if (!restoreGeometry(settings.value("MainWindowGeometry").toByteArray())) {
+        // Restore failed (perhaps missing setting), center the window
+        move(QApplication::desktop()->availableGeometry().center() - frameGeometry().center());
+    }
 
     QString windowTitle = tr("Dynamic") + " - ";
 #ifdef ENABLE_WALLET
@@ -279,8 +283,9 @@ DynamicGUI::~DynamicGUI()
     // Unsubscribe from notifications from core
     unsubscribeFromCoreSignals();
 
-    GUIUtil::saveWindowGeometry("nWindow", this);
-    if (trayIcon) // Hide tray icon, as deleting will let it linger until quit (on Ubuntu)
+    QSettings settings;
+    settings.setValue("MainWindowGeometry", saveGeometry());
+    if(trayIcon) // Hide tray icon, as deleting will let it linger until quit (on Ubuntu)
         trayIcon->hide();
 #ifdef Q_OS_MAC
     delete appMenuBar;
