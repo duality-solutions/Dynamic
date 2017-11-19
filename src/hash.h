09fc3d57ca85ea56fb49013c2ec3c99c5e96a1fe
Spencer Lievens <spencerlievens@users.noreply.github.com>
2017-11-19 21:08:44 +0100

    Update hash.h

--- src/hash.h
+++ src/hash.h
@@ -273,9 +273,9 @@ inline int Argon2d_Phase1_Hash(const void *in, const size_t size, const void *ou
     context.flags = DEFAULT_ARGON2_FLAG; // = ARGON2_DEFAULT_FLAGS
     // main configurable Argon2 hash parameters
     context.m_cost = 1000; // Memory in KiB
-    context.lanes = 64;    // Degree of Parallelism
+    context.lanes = 8;    // Degree of Parallelism
     context.threads = 1;  // Threads
-    context.t_cost = 8;   // Iterations
+    context.t_cost = 3;   // Iterations
 
     return argon2_ctx(&context, Argon2_d);
 }
