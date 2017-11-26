ff71a70ad48a5e25379c6b88ec06e60adbb6e35d
amirabrams <amirabrams@mail.com>
2017-11-26 14:40:18 -0600

    [Fluid] Correct max reward amounts

--- src/fluid.h
+++ src/fluid.h
@@ -29,8 +29,8 @@ public:
     static const int FLUID_ACTIVATE_HEIGHT = 10;
     static const int MAX_FLUID_TIME_DISTORT = 60 * 60; // Maximum time distort = 1 hour.
     static const int FEE_REDIRECT_HEIGHT = 100; //TODO (Amir): Change this to 250000
-    static const CAmount MAX_FLUID_MINER_REWARD = 3; // Maximum DYN miner block reward amount 
-    static const CAmount MAX_FLUID_DYNODE_REWARD = 5; // Maximum DYN Dynode block reward amount 
+    static const CAmount MAX_FLUID_MINER_REWARD = 3 * COIN; // Maximum DYN miner block reward amount 
+    static const CAmount MAX_FLUID_DYNODE_REWARD = 5 * COIN; // Maximum DYN Dynode block reward amount 
     static const CAmount MAX_FLUID_DYNODE_RECIPIENT_COUNT = 5; // Maximum Dynode recipient count per block
 
     //TODO (Amir): Change address and remove private keys below.
