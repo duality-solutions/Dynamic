a838e7ee6463ffadd3257b5009e081fa403600b9
spencerlievens <spencer.lievens@sterlingcoin.org.uk>
2017-12-03 11:40:36 +0100

    Further Typos

--- src/threadinterrupt.h
+++ src/threadinterrupt.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_THREADINTERRUPT_H
-#define BITCOIN_THREADINTERRUPT_H
+#ifndef DYNAMIC_THREADINTERRUPT_H
+#define DYNAMIC_THREADINTERRUPT_H
 
 #include <atomic>
 #include <chrono>
@@ -31,4 +31,4 @@ private:
     std::atomic<bool> flag;
 };
 
-#endif //BITCOIN_THREADINTERRUPT_H
+#endif //DYNAMIC_THREADINTERRUPT_H
