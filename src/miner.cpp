40f1f6c43a22a855795ab311f1418145894dad2a
spencerlievens <spencer.lievens@sterlingcoin.org.uk>
2017-12-05 16:21:23 +0100

    ENABLE_WALLET instances

--- src/miner.cpp
+++ src/miner.cpp
@@ -168,6 +168,7 @@ void FormatHashBuffers(CBlock* pblock, char* pmidstate, char* pdata, char* phash
     memcpy(phash1, &tmp.hash1, 64);
 }
 
+#ifdef ENABLE_WALLET
 bool CheckWork(const CChainParams& chainparams, CBlock* pblock, CWallet& wallet, CReserveKey& reservekey, CConnman* connman)
 {
     uint256 hash = pblock->GetHash();
@@ -199,6 +200,7 @@ bool CheckWork(const CChainParams& chainparams, CBlock* pblock, CWallet& wallet,
 
     return true;
 }
+#endif //ENABLE_WALLET
 
 std::unique_ptr<CBlockTemplate> CreateNewBlock(const CChainParams& chainparams, const CScript& scriptPubKeyIn)
 {
