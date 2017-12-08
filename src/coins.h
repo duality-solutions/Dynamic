4b2f4fc5670b590286b19b64b6063a965bf8f641
spencerlievens <spencer.lievens@sterlingcoin.org.uk>
2017-12-08 09:40:49 +0100

    Remove undefined FetchCoins method declaration

--- src/coins.h
+++ src/coins.h
@@ -492,7 +492,6 @@ public:
     friend class CCoinsModifier;
 
 private:
-    CCoinsMap::iterator FetchCoins(const uint256 &txid);
     CCoinsMap::const_iterator FetchCoins(const uint256 &txid) const;
 
     /**
