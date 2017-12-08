8f3ba4ff47f7c1fc78497a33813a073279520f76
spencerlievens <spencer.lievens@sterlingcoin.org.uk>
2017-12-08 11:49:15 +0100

    Few Minor per-utxo assert-semantics re-adds and tweak

--- src/coins.cpp
+++ src/coins.cpp
@@ -94,9 +94,9 @@ void AddCoins(CCoinsViewCache& cache, const CTransaction &tx, int nHeight) {
     }
 }
 
-void CCoinsViewCache::SpendCoin(const COutPoint &outpoint, Coin* moveout) {
+bool CCoinsViewCache::SpendCoin(const COutPoint &outpoint, Coin* moveout) {
     CCoinsMap::iterator it = FetchCoin(outpoint);
-    if (it == cacheCoins.end()) return;
+    if (it == cacheCoins.end()) return false;
     cachedCoinsUsage -= it->second.coin.DynamicMemoryUsage();
     if (moveout) {
         *moveout = std::move(it->second.coin);
