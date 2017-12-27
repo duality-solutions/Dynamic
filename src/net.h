d47b4085a6f3a8b1bb1d8d2cca59c42c83db1642
spencerlievens <spencer.lievens@sterlingcoin.org.uk>
2017-12-27 13:56:43 +0100

    Send non-tx/non-block inventory items

--- src/net.h
+++ src/net.h
@@ -754,6 +754,8 @@ public:
     // There is no final sorting before sending, as they are always sent immediately
     // and in the order requested.
     std::vector<uint256> vInventoryBlockToSend;
+    // List of non-tx/non-block inventory items
+    std::vector<CInv> vInventoryOtherToSend;
     CCriticalSection cs_inventory;
     std::set<uint256> setAskFor;
     std::multimap<int64_t, CInv> mapAskFor;
@@ -898,6 +900,9 @@ public:
         } else if (inv.type == MSG_BLOCK) {
             LogPrint("net", "PushInventory --  inv: %s peer=%d\n", inv.ToString(), id);
             vInventoryBlockToSend.push_back(inv.hash);
+        } else {
+            LogPrint("net", "PushInventory --  inv: %s peer=%d\n", inv.ToString(), id);
+            vInventoryOtherToSend.push_back(inv);
         }
     }
 
