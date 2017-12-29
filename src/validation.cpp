ab8a600db30f369a3aa06ba650fe64f17b8ba46a
spencerlievens <spencer.lievens@sterlingcoin.org.uk>
2017-12-29 09:06:31 +0100

    remove unneeded logging

--- src/validation.cpp
+++ src/validation.cpp
@@ -2444,11 +2444,6 @@ void static UpdateTip(CBlockIndex *pindexNew, const CChainParams& chainParams)
     // New best block
     mempool.AddTransactionsUpdated(1);
 
-    LogPrintf("%s: new best=%s  height=%d  log2_work=%.8g  tx=%lu  date=%s progress=%f  cache=%.1fMiB(%utxo)\n", __func__,
-      chainActive.Tip()->GetBlockHash().ToString(), chainActive.Height(), log(chainActive.Tip()->nChainWork.getdouble())/log(2.0), (unsigned long)chainActive.Tip()->nChainTx,
-      DateTimeStrFormat("%Y-%m-%d %H:%M:%S", chainActive.Tip()->GetBlockTime()),
-      Checkpoints::GuessVerificationProgress(chainParams.Checkpoints(), chainActive.Tip()), pcoinsTip->DynamicMemoryUsage() * (1.0 / (1<<20)), pcoinsTip->GetCacheSize());
-
     cvBlockChange.notify_all();
 
     static bool fWarned = false;
