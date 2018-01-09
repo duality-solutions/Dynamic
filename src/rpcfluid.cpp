0d24714c97050e2af1a5a51b2b2bedcb1f992629
spencerlievens <spencer.lievens@sterlingcoin.org.uk>
2018-01-09 07:50:24 +0100

    Remove MIT license from rpcfluid.cpp

--- src/rpcfluid.cpp
+++ src/rpcfluid.cpp
@@ -1,6 +1,4 @@
 // Copyright (c) 2017 Duality Blockchain Solutions Developers
-// Distributed under the MIT/X11 software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "fluid.h"
 
