023daf17bd3db957ae327861cee0c0b53c143bb7
Duality-CDOO <s.lievens@duality.solutions>
2018-04-10 23:30:33 +0200

    Bump LevelDB/UniValue/secp256k1 versions

--- src/univalue/test/unitester.cpp
+++ src/univalue/test/unitester.cpp
@@ -17,6 +17,7 @@
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 #endif
 
+using namespace std;
 string srcdir(JSON_TEST_SRC);
 static bool test_failed = false;
 
@@ -31,7 +32,7 @@ static std::string rtrim(std::string s)
 
 static void runtest(string filename, const string& jdata)
 {
-        std::string prefix = filename.substr(0, 4);
+        string prefix = filename.substr(0, 4);
 
         bool wantPass = (prefix == "pass") || (prefix == "roun");
         bool wantFail = (prefix == "fail");
@@ -55,12 +56,12 @@ static void runtest(string filename, const string& jdata)
 
 static void runtest_file(const char *filename_)
 {
-        std::string basename(filename_);
-        std::string filename = srcdir + "/" + basename;
+        string basename(filename_);
+        string filename = srcdir + "/" + basename;
         FILE *f = fopen(filename.c_str(), "r");
         assert(f != NULL);
 
-        std::string jdata;
+        string jdata;
 
         char buf[4096];
         while (!feof(f)) {
@@ -112,6 +113,8 @@ static const char *filenames[] = {
         "fail39.json",               // invalid unicode: only second half of surrogate pair
         "fail40.json",               // invalid unicode: broken UTF-8
         "fail41.json",               // invalid unicode: unfinished UTF-8
+        "fail42.json",               // valid json with garbage following a nul byte
+        "fail44.json",               // unterminated string
         "fail3.json",
         "fail4.json",                // extra comma
         "fail5.json",
@@ -124,6 +127,11 @@ static const char *filenames[] = {
         "pass3.json",
         "round1.json",              // round-trip test
         "round2.json",              // unicode
+        "round3.json",              // bare string
+        "round4.json",              // bare number
+        "round5.json",              // bare true
+        "round6.json",              // bare false
+        "round7.json",              // bare null
 };
 
 // Test \u handling
