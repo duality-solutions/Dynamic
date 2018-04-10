026d8c8d55e331ca0427d2c4f751fffdcad6e3bf
Duality-CDOO <s.lievens@duality.solutions>
2018-04-10 17:16:18 +0200

    [Qt] RPC-Console: support nested commands and simple value queries

--- src/qt/rpcconsole.h
+++ src/qt/rpcconsole.h
@@ -38,6 +38,8 @@ public:
     explicit RPCConsole(QWidget *parent);
     ~RPCConsole();
 
+    static bool RPCExecuteCommandLine(std::string &strResult, const std::string &strCommand);
+
     void setClientModel(ClientModel *model);
 
     enum MessageClass {
