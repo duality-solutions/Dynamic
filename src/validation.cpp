ea5e54d91339501b9d338872ad3216b89b534010
Duality-CDOO <s.lievens@duality.solutions>
2018-04-11 20:10:36 +0200

    Trivial: Explicitly pass const CChainParams& to LoadBlockIndexDB()

--- src/validation.cpp
+++ src/validation.cpp
@@ -3803,9 +3803,8 @@ CBlockIndex * InsertBlockIndex(uint256 hash)
     return pindexNew;
 }
 
-bool static LoadBlockIndexDB()
+bool static LoadBlockIndexDB(const CChainParams& chainparams)
 {
-    const CChainParams& chainparams = Params();
     if (!pblocktree->LoadBlockIndexGuts(InsertBlockIndex))
         return false;
 
@@ -4055,10 +4054,10 @@ void UnloadBlockIndex()
     fHavePruned = false;
 }
 
-bool LoadBlockIndex()
+bool LoadBlockIndex(const CChainParams& chainparams)
 {
     // Load block index from databases
-    if (!fReindex && !LoadBlockIndexDB())
+    if (!fReindex && !LoadBlockIndexDB(chainparams))
         return false;
     return true;
 }
