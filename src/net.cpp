e808abdf7c05d1d59c4594b23e8aa6f060bde720
Duality-CDOO <s.lievens@duality.solutions>
2018-04-11 13:52:27 +0200

    Make connect=0 disable automatic outbound connections.

--- src/net.cpp
+++ src/net.cpp
@@ -2249,8 +2249,9 @@ bool CConnman::Start(CScheduler& scheduler, std::string& strNodeError, Options c
     // Initiate outbound connections from -addnode
     threadOpenAddedConnections = std::thread(&TraceThread<std::function<void()> >, "addcon", std::function<void()>(std::bind(&CConnman::ThreadOpenAddedConnections, this)));
 
-    // Initiate outbound connections
-    threadOpenConnections = std::thread(&TraceThread<std::function<void()> >, "opencon", std::function<void()>(std::bind(&CConnman::ThreadOpenConnections, this)));
+    // Initiate outbound connections unless connect=0
+    if (!mapArgs.count("-connect") || mapMultiArgs["-connect"].size() != 1 || mapMultiArgs["-connect"][0] != "0")
+        threadOpenConnections = std::thread(&TraceThread<std::function<void()> >, "opencon", std::function<void()>(std::bind(&CConnman::ThreadOpenConnections, this)));
 
     // Initiate dynode connections
     threadDnbRequestConnections = std::thread(&TraceThread<std::function<void()> >, "dnbcon", std::function<void()>(std::bind(&CConnman::ThreadDnbRequestConnections, this)));
