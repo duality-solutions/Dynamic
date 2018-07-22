792f1511c981951b7ed0bd5f2a1f938c6d3b0e44
Duality-CDOO <s.lievens@duality.solutions>
2018-07-22 23:49:17 +0200

    Do not create dnb until dynodeSync is finished

--- src/dynode.cpp
+++ src/dynode.cpp
@@ -338,8 +338,8 @@ bool CDynodeBroadcast::Create(std::string strService, std::string strKeyDynode, 
         return false;
     };
 
-    //need correct blocks to send ping
-    if (!fOffline && !dynodeSync.IsBlockchainSynced())
+    // Wait for sync to finish because dnb simply won't be relayed otherwise
+    if (!fOffline && !dynodeSync.IsSynced())
         return Log("Sync in progress. Must wait until sync is complete to start Dynode");
 
     if (!CMessageSigner::GetKeysFromSecret(strKeyDynode, keyDynodeNew, pubKeyDynodeNew))
