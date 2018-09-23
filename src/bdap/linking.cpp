89d86c3da30b29b79a6400a729b353193387220e
AmirAbrams <amirabrams@mail.com>
2018-09-23 03:12:36 -0500

    [BDAP] Add recipient link and accept link classes

---
+++ src/bdap/linking.cpp
@@ -0,0 +1,121 @@
+// Copyright (c) 2018 Duality Blockchain Solutions Developers
+// TODO: license
+
+#include "bdap/linking.h"
+
+#include "bdap/domainentry.h"
+#include "hash.h"
+#include "script/script.h"
+#include "streams.h"
+
+bool CRequestLink::UnserializeFromTx(const CTransaction& tx) 
+{
+    std::vector<unsigned char> vchData;
+    std::vector<unsigned char> vchHash;
+    int nOut;
+    if(!GetBDAPData(tx, vchData, vchHash, nOut))
+    {
+        SetNull();
+        return false;
+    }
+    if(!UnserializeFromData(vchData, vchHash))
+    {
+        return false;
+    }
+    return true;
+}
+
+void CRequestLink::Serialize(std::vector<unsigned char>& vchData) 
+{
+    CDataStream dsRequestLink(SER_NETWORK, PROTOCOL_VERSION);
+    dsRequestLink << *this;
+    vchData = std::vector<unsigned char>(dsRequestLink.begin(), dsRequestLink.end());
+}
+
+bool CRequestLink::UnserializeFromData(const std::vector<unsigned char>& vchData, const std::vector<unsigned char>& vchHash) 
+{
+    try {
+        CDataStream dsRequestLink(vchData, SER_NETWORK, PROTOCOL_VERSION);
+        dsRequestLink >> *this;
+
+        std::vector<unsigned char> vchRequestLinkData;
+        Serialize(vchRequestLinkData);
+        const uint256 &calculatedHash = Hash(vchRequestLinkData.begin(), vchRequestLinkData.end());
+        const std::vector<unsigned char> &vchRandRequestLink = vchFromValue(calculatedHash.GetHex());
+        if(vchRandRequestLink != vchHash)
+        {
+            SetNull();
+            return false;
+        }
+    } catch (std::exception &e) {
+        SetNull();
+        return false;
+    }
+    return true;
+}
+
+CharString CRequestLink::SharedSignPublicKey()
+{
+    CharString vchReturn;
+    return vchReturn;
+}
+
+bool CRequestLink::ValidateValues(std::string& errorMessage)
+{
+    return true;
+}
+
+
+bool CRequestLink::IsMyLinkRequest(const CTransaction& tx)
+{
+    return false;
+}
+
+void CAcceptLink::Serialize(std::vector<unsigned char>& vchData) 
+{
+    CDataStream dsAcceptLink(SER_NETWORK, PROTOCOL_VERSION);
+    dsAcceptLink << *this;
+    vchData = std::vector<unsigned char>(dsAcceptLink.begin(), dsAcceptLink.end());
+}
+
+bool CAcceptLink::UnserializeFromData(const std::vector<unsigned char>& vchData, const std::vector<unsigned char>& vchHash) 
+{
+    try {
+        CDataStream dsAcceptLink(vchData, SER_NETWORK, PROTOCOL_VERSION);
+        dsAcceptLink >> *this;
+
+        std::vector<unsigned char> vchAcceptLinkData;
+        Serialize(vchAcceptLinkData);
+        const uint256 &calculatedHash = Hash(vchAcceptLinkData.begin(), vchAcceptLinkData.end());
+        const std::vector<unsigned char> &vchRandAcceptLink = vchFromValue(calculatedHash.GetHex());
+        if(vchRandAcceptLink != vchHash)
+        {
+            SetNull();
+            return false;
+        }
+    } catch (std::exception &e) {
+        SetNull();
+        return false;
+    }
+    return true;
+}
+
+bool CAcceptLink::ValidateValues(std::string& errorMessage)
+{
+    // TODO: (BDAP) implement validation
+    return true;
+}
+
+// TODO: (BDAP) implement BDAP encryption by converting ed25519 private key to curve25519
+/*
+CharString GetEncryptedLinkMessage(const CRequestLink& requestLink)
+{
+
+}
+
+CharString GetEncryptedLinkMessage(const CRequestLink& requestLink, const CAcceptLink& acceptLink)
+{
+
+
+}
+*/
---
+++ src/bdap/linking.h
@@ -0,0 +1,173 @@
+// Copyright (c) 2018 Duality Blockchain Solutions Developers
+// TODO: license
+
+#ifndef DYNAMIC_BDAP_LINKING_H
+#define DYNAMIC_BDAP_LINKING_H
+
+#include "bdap.h"
+#include "serialize.h"
+#include "uint256.h"
+
+class CTransaction;
+
+// CRequestLink are stored serilzed and encrypted in a BDAP OP_RETURN transaction.
+// The link request recipient can decrypt the BDAP OP_RETURN transaction 
+// and get the needed information to accept the link request
+// It is used to bootstrap the linkage relationship with a new set of public keys
+
+// OP_RETURN Format:
+// CAcceptLink.RecipientPublicKey.Encrypt(CRequestLink.SharedSymmetricPrivKey).ToHex() + space +
+// CRequestLink.RequestorPublicKey.Encrypt(CRequestLink.SharedSymmetricPrivKey).ToHex() + spcae + 
+// CRequestLink.SharedSymmetricPrivKey.Encrypt(Serialize(CRequestLink)).ToHex()
+class CRequestLink {
+public:
+    static const int CURRENT_VERSION=1;
+    int nVersion;
+    CharString RequestorFullObjectPath; // Requestor's BDAP object path
+    CharString RecipientFullObjectPath; // Recipient's BDAP object path
+    // The SharedPrivateKey will allow both parties to update mutable entries on DHT torrent network 
+    CharString SharedPrivateKey; // ed25519 derived key new/unique for this link
+    CharString RequestorPublicKey; // ed25519 derived public key new/unique for this link
+    CharString SharedSymmetricPrivKey; // AES symmetric private key unique to this link request
+
+    unsigned int nHeight;
+    uint64_t nExpireTime;
+    uint256 txHash;
+
+    CRequestLink() { 
+        SetNull();
+    }
+
+    CRequestLink(const CTransaction &tx) {
+        SetNull();
+        UnserializeFromTx(tx);
+    }
+
+    inline void SetNull()
+    {
+        nVersion = CRequestLink::CURRENT_VERSION;
+        RequestorFullObjectPath.clear();
+        RecipientFullObjectPath.clear();
+        SharedPrivateKey.clear();
+        RequestorPublicKey.clear();
+        SharedSymmetricPrivKey.clear();
+        nHeight = 0;
+        nExpireTime = 0;
+        txHash.SetNull();
+    }
+
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action) {
+        READWRITE(this->nVersion);
+        READWRITE(RequestorFullObjectPath);
+        READWRITE(RecipientFullObjectPath);
+        READWRITE(SharedPrivateKey);
+        READWRITE(RequestorPublicKey);
+        READWRITE(SharedSymmetricPrivKey);
+        READWRITE(VARINT(nHeight));
+        READWRITE(VARINT(nExpireTime));
+        READWRITE(txHash);
+    }
+
+    inline friend bool operator==(const CRequestLink &a, const CRequestLink &b) {
+        return (a.RequestorFullObjectPath == b.RequestorFullObjectPath && a.RecipientFullObjectPath == b.RecipientFullObjectPath 
+                    && a.SharedPrivateKey == b.SharedPrivateKey && a.RequestorPublicKey == b.RequestorPublicKey
+                    && a.SharedSymmetricPrivKey == b.SharedSymmetricPrivKey);
+    }
+
+    inline friend bool operator!=(const CRequestLink &a, const CRequestLink &b) {
+        return !(a == b);
+    }
+
+    inline CRequestLink operator=(const CRequestLink &b) {
+        nVersion = b.nVersion;
+        RequestorFullObjectPath = b.RequestorFullObjectPath;
+        RecipientFullObjectPath = b.RecipientFullObjectPath;
+        SharedPrivateKey = b.SharedPrivateKey;
+        RequestorPublicKey = b.RequestorPublicKey;
+        SharedSymmetricPrivKey = b.SharedSymmetricPrivKey;
+        nHeight = b.nHeight;
+        nExpireTime = b.nExpireTime;
+        txHash = b.txHash;
+        return *this;
+    }
+ 
+    inline bool IsNull() const { return (RequestorFullObjectPath.empty()); }
+    bool UnserializeFromTx(const CTransaction &tx);
+    bool UnserializeFromData(const std::vector<unsigned char> &vchData, const std::vector<unsigned char> &vchHash);
+    void Serialize(std::vector<unsigned char>& vchData);
+
+    CharString SharedSignPublicKey(); //derive from SharedPrivateKey
+    bool ValidateValues(std::string& errorMessage);
+    bool IsMyLinkRequest(const CTransaction& tx);
+};
+
+// CAcceptLink are stored serilzed and encrypted in a LibTorrent DHT key value pair entry
+// Stored in the Torrent DHT for a limited time.
+// This is only used when the link recipient wants to accepts the request.
+
+// DHT Data Format (must be under 1000 bytes):
+// CRequestLink.RequestorPublicKey.Encrypt(CRequestLink.SharedSymmetricPrivKey).ToHex() + spcae + 
+// CAcceptLink.RecipientPublicKey.Encrypt(CRequestLink.SharedSymmetricPrivKey).ToHex() + space +
+// CRequestLink.SharedSymmetricPrivKey.Encrypt(Serialize(CAcceptLink)).ToHex()
+class CAcceptLink {
+public:
+    static const int CURRENT_VERSION=1;
+    int nVersion;
+    CharString RequestorFullObjectPath; // Requestor's BDAP object path
+    CharString RecipientFullObjectPath; // Recipient's BDAP object path
+    CharString RecipientPublicKey; // ed25519 derived public key new/unique for this link
+
+    CAcceptLink() {
+        SetNull();
+    }
+
+    inline void SetNull()
+    {
+        nVersion = CAcceptLink::CURRENT_VERSION;
+        RequestorFullObjectPath.clear();
+        RecipientFullObjectPath.clear();
+        RecipientPublicKey.clear();
+    }
+
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action) {
+        READWRITE(this->nVersion);
+        READWRITE(RequestorFullObjectPath);
+        READWRITE(RecipientFullObjectPath);
+        READWRITE(RecipientPublicKey);
+    }
+
+    inline friend bool operator==(const CAcceptLink &a, const CAcceptLink &b) {
+        return (a.RequestorFullObjectPath == b.RequestorFullObjectPath && a.RecipientFullObjectPath == b.RecipientFullObjectPath
+                    && a.RecipientPublicKey == b.RecipientPublicKey);
+    }
+
+    inline friend bool operator!=(const CAcceptLink &a, const CAcceptLink &b) {
+        return !(a == b);
+    }
+
+    inline CAcceptLink operator=(const CAcceptLink &b) {
+        nVersion = b.nVersion;
+        RequestorFullObjectPath = b.RequestorFullObjectPath;
+        RecipientFullObjectPath = b.RecipientFullObjectPath;
+        RecipientPublicKey = b.RecipientPublicKey;
+        return *this;
+    }
+ 
+    inline bool IsNull() const { return (RequestorFullObjectPath.empty()); }
+    bool UnserializeFromData(const std::vector<unsigned char> &vchData, const std::vector<unsigned char> &vchHash);
+    void Serialize(std::vector<unsigned char>& vchData);
+
+    bool ValidateValues(std::string& errorMessage);
+};
+
+// TODO (BDAP): Implement
+CharString GetEncryptedRequestMessage(const CRequestLink& requestLink); // stored in an OP_RETURN transaction
+CharString GetEncryptedAcceptMessage(const CRequestLink& requestLink, const CAcceptLink& acceptLink); // stored on BitTorrent DHT network
+
+#endif // DYNAMIC_BDAP_LINKING_H
