c821539bb5300b88315f6a47ab71a85be7caae13
AmirAbrams <amirabrams@mail.com>
2018-11-14 12:01:47 -0600

    [DHT] Cleanup storage debug.log usage

--- src/dht/storage.cpp
+++ src/dht/storage.cpp
@@ -27,46 +27,45 @@ using namespace libtorrent::dht;
 
 size_t CDHTStorage::num_torrents() const
 { 
-    LogPrintf("********** CDHTStorage -- num_torrents **********\n");
+    LogPrint("dht", "CDHTStorage -- num_torrents\n");
     return pDefaultStorage->num_torrents(); 
 }
 
 size_t CDHTStorage::num_peers() const
 {
-    LogPrintf("********** CDHTStorage -- num_peers **********\n");
+    LogPrint("dht", "CDHTStorage -- num_peers\n");
     return pDefaultStorage->num_peers(); 
 }
 
 void CDHTStorage::update_node_ids(std::vector<libtorrent::sha1_hash> const& ids)
 {
-    LogPrintf("********** CDHTStorage -- update_node_ids **********\n");
+    LogPrint("dht", "CDHTStorage -- update_node_ids\n");
     pDefaultStorage->update_node_ids(ids);
 }
 
 bool CDHTStorage::get_peers(sha1_hash const& info_hash, bool const noseed, bool const scrape, address const& requester, entry& peers) const
 {
     bool ret = pDefaultStorage->get_peers(info_hash, noseed, scrape, requester, peers);
-    //LogPrintf("********** CDHTStorage -- get_peers peers = %s **********\n", peers.to_string());
+    //LogPrint("dht", "CDHTStorage -- get_peers peers = %s **********\n", peers.to_string());
     return ret;
 }
 
 void CDHTStorage::announce_peer(sha1_hash const& info_hash, tcp::endpoint const& endp, string_view name, bool const seed)
 {
-    LogPrintf("********** CDHTStorage -- announce_peer **********\n");
+    LogPrint("dht", "CDHTStorage -- announce_peer\n");
     pDefaultStorage->announce_peer(info_hash, endp, name, seed);
 }
 
 bool CDHTStorage::get_immutable_item(sha1_hash const& target, entry& item) const
 {
+    LogPrint("dht", "CDHTStorage -- get_immutable_item\n");
     return false;
 }
 
 void CDHTStorage::put_immutable_item(sha1_hash const& target, span<char const> buf, address const& addr)
 {
     //TODO: ban nodes that try to put immutable entries.
-    //pDefaultStorage->put_immutable_item(target, buf, addr);
-    // Do we need immutable item support ???
-    LogPrintf("********** CDHTStorage -- put_immutable_item target = %s, buf = %s, addr = %s\n", aux::to_hex(target.to_string()), std::string(buf.data()), addr.to_string());
+    LogPrint("dht", "CDHTStorage -- put_immutable_item target = %s, buf = %s, addr = %s\n", aux::to_hex(target.to_string()), std::string(buf.data()), addr.to_string());
 }
 
 bool CDHTStorage::get_mutable_item_seq(sha1_hash const& target, sequence_number& seq) const
@@ -77,13 +76,13 @@ bool CDHTStorage::get_mutable_item_seq(sha1_hash const& target, sequence_number&
     CMutableData mutableData;
     std::string strInfoHash = aux::to_hex(target.to_string());
     CharString vchInfoHash = vchFromString(strInfoHash);
-    LogPrintf("********** CDHTStorage -- get_mutable_item_seq infohash = %s\n", strInfoHash);
+    LogPrint("dht", "CDHTStorage -- get_mutable_item_seq infohash = %s\n", strInfoHash);
     if (!GetLocalMutableData(vchInfoHash, mutableData)) {
         LogPrintf("********** CDHTStorage -- get_mutable_item_seq failed to get mutable entry sequence_number for infohash = %s.\n", strInfoHash);
         return false;
     }
     seq = dht::sequence_number(mutableData.SequenceNumber);
-    LogPrintf("********** CDHTStorage -- get_mutable_item_seq found seq = %u\n", mutableData.SequenceNumber);
+    LogPrint("dht", "CDHTStorage -- get_mutable_item_seq found seq = %u\n", mutableData.SequenceNumber);
     return true;
 }
 
@@ -111,7 +110,7 @@ bool CDHTStorage::get_mutable_item(sha1_hash const& target, sequence_number cons
         aux::from_hex(mutableData.PublicKey(), pubKey.data());
         item["k"] = pubKey;
     }
-    LogPrintf("********** CDHTStorage -- get_mutable_item target = %s, item = %s\n", aux::to_hex(target.to_string()), item.to_string());
+    LogPrint("dht", "CDHTStorage -- get_mutable_item target = %s, item = %s\n", aux::to_hex(target.to_string()), item.to_string());
     return true;
 }
 
@@ -152,46 +151,45 @@ void CDHTStorage::put_mutable_item(sha1_hash const& target
     CharString vchSalt = vchFromString(strSalt);
 
     CMutableData putMutableData(vchInfoHash, vchPublicKey, vchSignature, seq.value, vchSalt, vchPutValue);
-    LogPrintf("********** CDHTStorage -- put_mutable_item info_hash = %s, buf_value = %s, salt = %s, seq = %d, put_size = %d, sig_size = %d, pubkey_size = %d, salt_size = %d\n", 
+    LogPrint("dht", "CDHTStorage -- put_mutable_item info_hash = %s, buf_value = %s, salt = %s, seq = %d, put_size = %d, sig_size = %d, pubkey_size = %d, salt_size = %d\n", 
                     strInfoHash, strPutValue, strSalt, putMutableData.SequenceNumber, 
                     vchPutValue.size(), vchSignature.size(), vchPublicKey.size(), vchSalt.size());
 
     CMutableData previousData;
     if (!GetLocalMutableData(vchInfoHash, previousData)) {
         if (PutLocalMutableData(vchInfoHash, putMutableData)) {
-            LogPrintf("********** CDHTStorage -- put_mutable_item added successfully**********\n");
+            LogPrint("dht", "CDHTStorage -- put_mutable_item added successfully\n");
         }
     }
     else {
         if (putMutableData.Value() != previousData.Value() || putMutableData.SequenceNumber != previousData.SequenceNumber) {
             if (UpdateLocalMutableData(vchInfoHash, putMutableData)) {
-                LogPrintf("********** CDHTStorage -- put_mutable_item updated successfully**********\n");
+                LogPrint("dht", "CDHTStorage -- put_mutable_item updated successfully\n");
             }
         }
         else {
-            LogPrintf("********** CDHTStorage -- put_mutable_item value unchanged. No database operation needed. **********\n");
+            LogPrint("dht", "CDHTStorage -- put_mutable_item value unchanged. No database operation needed.\n");
         }
     }
     // TODO: Log from address (addr). See touch_item in the default storage implementation.
     return;
-    
 }
 
 int CDHTStorage::get_infohashes_sample(entry& item)
 {
-    LogPrintf("********** CDHTStorage -- get_infohashes_sample **********\n");
+    LogPrint("dht", "CDHTStorage -- get_infohashes_sample\n");
     return pDefaultStorage->get_infohashes_sample(item);
 }
 
 void CDHTStorage::tick()
 {
-    LogPrintf("********** CDHTStorage -- tick **********\n");
+    LogPrint("dht", "CDHTStorage -- tick\n");
     pDefaultStorage->tick();
 }
 
 dht_storage_counters CDHTStorage::counters() const
 {
-    LogPrintf("********** CDHTStorage -- counters **********\n");
+    LogPrint("dht", "CDHTStorage -- counters\n");
     return pDefaultStorage->counters();
 }
 
