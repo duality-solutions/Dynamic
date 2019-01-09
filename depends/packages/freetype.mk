7a0469e42eb72c626a13483d81e4da069be33246
Sven Kercher <24934984+SvenKercher@users.noreply.github.com>
2019-01-09 21:08:36 +0100

    [DEPENDS] fix freetype hash

--- depends/packages/freetype.mk
+++ depends/packages/freetype.mk
@@ -2,7 +2,7 @@ package=freetype
 $(package)_version=2.9.1
 $(package)_download_path=http://download.savannah.gnu.org/releases/$(package)
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=aa2f835ef8f50072630ddc48b9eb65f1f456014ffa3b5adddcb6bf390a3c5828
+$(package)_sha256_hash=db8d87ea720ea9d5edc5388fc7a0497bb11ba9fe972245e0f7f4c7e8b1e1e84d
 
 define $(package)_set_vars
   $(package)_config_opts=--without-zlib --without-png --disable-static
