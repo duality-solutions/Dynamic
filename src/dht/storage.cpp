8425cc95ffa1d7cac241ec2088deb136436e79ad
Amir Abrams <amirabrams@mail.com>
2019-04-18 23:44:48 -0500

    [DHT] Only Dynode respond to get and put storage requests

--- src/dht/storage.cpp
+++ src/dht/storage.cpp
@@ -40,12 +40,16 @@ size_t CDHTStorage::num_peers() const
 
 void CDHTStorage::update_node_ids(std::vector<libtorrent::sha1_hash> const& ids)
 {
+    if (!fDynodeMode) // Only update node ids if Dynode
+        return;
     LogPrint("dht", "CDHTStorage -- update_node_ids\n");
     pDefaultStorage->update_node_ids(ids);
 }
 
 bool CDHTStorage::get_peers(sha1_hash const& info_hash, bool const noseed, bool const scrape, address const& requester, entry& peers) const
 {
+    if (!fDynodeMode) // Only get peers if Dynode
+        return false;
     bool ret = pDefaultStorage->get_peers(info_hash, noseed, scrape, requester, peers);
     //LogPrint("dht", "CDHTStorage -- get_peers peers = %s **********\n", peers.to_string());
     return ret;
@@ -53,6 +57,8 @@ bool CDHTStorage::get_peers(sha1_hash const& info_hash, bool const noseed, bool 
 
 void CDHTStorage::announce_peer(sha1_hash const& info_hash, tcp::endpoint const& endp, string_view name, bool const seed)
 {
+    if (!fDynodeMode) // Only announce peers if Dynode
+        return;
     LogPrint("dht", "CDHTStorage -- announce_peer\n");
     pDefaultStorage->announce_peer(info_hash, endp, name, seed);
 }
@@ -71,6 +77,8 @@ void CDHTStorage::put_immutable_item(sha1_hash const& target, span<char const> b
 
 bool CDHTStorage::get_mutable_item_seq(sha1_hash const& target, sequence_number& seq) const
 {
+    if (!fDynodeMode) // Only try to get DHT data if Dynode
+        return false;
     //bool ret = pDefaultStorage->get_mutable_item_seq(target, seq);
     //return ret;
     // TODO (DHT): Try to find entry in memory before searching leveldb
@@ -89,6 +97,8 @@ bool CDHTStorage::get_mutable_item_seq(sha1_hash const& target, sequence_number&
 
 bool CDHTStorage::get_mutable_item(sha1_hash const& target, sequence_number const seq, bool const force_fill, entry& item) const
 {
+    if (!fDynodeMode) // Only try to get DHT data if Dynode
+        return false;
     //bool ret = pDefaultStorage->get_mutable_item(target, seq, force_fill, item);
     //return ret;
     // TODO (DHT): Try to find entry in memory before searching leveldb
@@ -130,8 +140,11 @@ void CDHTStorage::put_mutable_item(sha1_hash const& target
     , span<char const> salt
     , address const& addr)
 {
+    if (!fDynodeMode) // Do not store DHT data if not a Dynode
+        return;
     // TODO (DHT): Store entries in memory as well
     //pDefaultStorage->put_mutable_item(target, buf, sig, seq, pk, salt, addr);
+
     std::string strInfoHash = aux::to_hex(target.to_string());
     CharString vchInfoHash = vchFromString(strInfoHash);
 
