3b6214d9fd0fd0e2514e27bd95e76401107e599a
Amir Abrams <amirabrams@mail.com>
2019-07-20 13:44:48 -0500

    Reduce max standard transaction to 100 KB

--- src/policy/policy.h
+++ src/policy/policy.h
@@ -22,7 +22,7 @@ static const unsigned int DEFAULT_BLOCK_PRIORITY_SIZE = 200000;
 /** Default for -blockmintxfee, which sets the minimum feerate for a transaction in blocks created by mining code **/
 static const unsigned int DEFAULT_BLOCK_MIN_TX_FEE = 1000;
 /** The maximum size for transactions we're willing to relay/mine */
-static const unsigned int MAX_STANDARD_TX_SIZE = 400000; // 4MB
+static const unsigned int MAX_STANDARD_TX_SIZE = 100000; // 100 KB
 /** Maximum number of signature check operations in an IsStandard() P2SH script */
 static const unsigned int MAX_P2SH_SIGOPS = 15;
 /** The maximum number of sigops we're willing to relay/mine in a single tx */
