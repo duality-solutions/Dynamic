bf8e46310b427656a58e44e5e32b74aabf764cf8
Kittywhiskers Van Gogh <6098974-kittywhiskers@users.noreply.gitlab.com>
2019-10-12 12:56:25 -0400

    gui: update BIP70 support message

--- src/qt/paymentserver.cpp
+++ src/qt/paymentserver.cpp
@@ -416,12 +416,18 @@ void PaymentServer::handleURIOrFile(const QString& s)
         } else // normal URI
         {
             SendCoinsRecipient recipient;
-            if (GUIUtil::parseDynamicURI(s, &recipient)) {
-                CDynamicAddress address(recipient.address.toStdString());
-                if (!address.IsValid()) {
-                    Q_EMIT message(tr("URI handling"), tr("Invalid payment address %1").arg(recipient.address),
-                        CClientUIInterface::MSG_ERROR);
-                } else
+            if (GUIUtil::parseDynamicURI(s, &recipient))
+            {
+                if (!IsValidDestinationString(recipient.address.toStdString())) {
+                    if (uri.hasQueryItem("r")) {  // payment request
+                        Q_EMIT message(tr("URI handling"),
+                            tr("Cannot process payment request because BIP70 is not supported.")+
+                            tr("Due to widespread security flaws in BIP70 it's strongly recommended that any merchant instructions to switch wallets be ignored.")+
+                            tr("If you are receiving this error you should request the merchant provide a BIP21 compatible URI."),
+                            CClientUIInterface::ICON_WARNING);
+                    }
+                }
+                else
                     Q_EMIT receivedPaymentRequest(recipient);
             } else
                 Q_EMIT message(tr("URI handling"),
@@ -434,16 +440,11 @@ void PaymentServer::handleURIOrFile(const QString& s)
 
     if (QFile::exists(s)) // payment request file
     {
-        PaymentRequestPlus request;
-        SendCoinsRecipient recipient;
-        if (!readPaymentRequestFromFile(s, request)) {
-            Q_EMIT message(tr("Payment request file handling"),
-                tr("Payment request file cannot be read! This can be caused by an invalid payment request file."),
-                CClientUIInterface::ICON_WARNING);
-        } else if (processPaymentRequest(request, recipient))
-            Q_EMIT receivedPaymentRequest(recipient);
-
-        return;
+      Q_EMIT message(tr("Payment request file handling"),
+          tr("Cannot process payment request because BIP70 is not supported.")+
+          tr("Due to widespread security flaws in BIP70 it's strongly recommended that any merchant instructions to switch wallets be ignored.")+
+          tr("If you are receiving this error you should request the merchant provide a BIP21 compatible URI."),
+          CClientUIInterface::ICON_WARNING);
     }
 }
 
