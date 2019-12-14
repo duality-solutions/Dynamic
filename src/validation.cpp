07f5b1c92d12e7478f1b1c2db9c566dbd4659f41
Amir Abrams <amirabrams@mail.com>
2019-12-14 03:17:33 -0600

    [PoS] Fix issues in CheckBlock when mining PoW blocks - Caused a segfault when the Proof-of-Work miner starts

--- src/validation.cpp
+++ src/validation.cpp
@@ -3882,7 +3882,7 @@ bool CheckBlock(const CBlock& block, CValidationState& state, const Consensus::P
 
     // Check that the header is valid (particularly PoW).  This is mostly
     // redundant with the call in AcceptBlockHeader.
-    if (!CheckBlockHeader(block, state, consensusParams, !IsPoS))
+    if (!CheckBlockHeader(block, state, consensusParams, fCheckPOW))
         return false;
 
     // Check the merkle root.
