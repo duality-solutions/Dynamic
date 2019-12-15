453128001869afe8c15a4beb097a349f01b0bbe4
Spencer Lievens <36229929+Duality-CDOO@users.noreply.github.com>
2019-12-15 10:59:03 +0100

    [PoS] Add Stake to BlocktoJSON

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -15,6 +15,7 @@
 #include "hash.h"
 #include "instantsend.h"
 #include "policy/policy.h"
+#include "pos/kernel.cpp"
 #include "primitives/transaction.h"
 #include "rpc/server.h"
 #include "streams.h"
@@ -138,6 +139,34 @@ UniValue blockToJSON(const CBlock& block, const CBlockIndex* blockindex, bool tx
     CBlockIndex* pnext = chainActive.Next(blockindex);
     if (pnext)
         result.push_back(Pair("nextblockhash", pnext->GetBlockHash().GetHex()));
+    
+    result.push_back(Pair("modifier", strprintf("%016x", blockindex->nStakeModifier)));
+    result.push_back(Pair("moneysupply",ValueFromAmount(blockindex->nMoneySupply)));
+
+    //////////
+    ////////// Coin stake data ////////////////
+    /////////
+    if (block.IsProofOfStake()) {
+        // First grab it
+        uint256 hashProofOfStakeRet;
+        std::unique_ptr <CStakeInput> stake;
+        // Initialize the stake object (we should look for this in some other place and not initialize it every time..)
+        if (!initStakeInput(block, stake, blockindex->nHeight - 1))
+            throw JSONRPCError(RPC_INTERNAL_ERROR, "Cannot initialize stake input");
+
+        unsigned int nTxTime = block.nTime;
+        // todo: Add the debug as param..
+        if (!GetHashProofOfStake(blockindex->pprev, stake.get(), nTxTime, false, hashProofOfStakeRet))
+            throw JSONRPCError(RPC_INTERNAL_ERROR, "Cannot get proof of stake hash");
+
+        UniValue stakeData(UniValue::VOBJ);
+        stakeData.push_back(Pair("BlockFromHash", stake.get()->GetIndexFrom()->GetBlockHash().GetHex()));
+        stakeData.push_back(Pair("BlockFromHeight", stake.get()->GetIndexFrom()->nHeight));
+        stakeData.push_back(Pair("hashProofOfStake", hashProofOfStakeRet.GetHex()));
+        stakeData.push_back(Pair("stakeModifierHeight", (std::to_string(stake->getStakeModifierHeight()))));
+        result.push_back(Pair("CoinStake", stakeData));
+    }
+
     return result;
 }
 
