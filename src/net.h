9d8f730f6dc4a268c7075953c29adbcead7b2fe2
Duality-CDOO <s.lievens@duality.solutions>
2020-06-10 09:47:01 +0200

    Add MAX_ASSET_INV_SZ to net.h

--- src/net.h
+++ src/net.h
@@ -59,6 +59,8 @@ static const int WARNING_INTERVAL = 10 * 60;
 static const int FEELER_INTERVAL = 120;
 /** The maximum number of entries in an 'inv' protocol message */
 static const unsigned int MAX_INV_SZ = 50000;
+/** The maximum number of entries in an 'asset inv' protocol message */
+static const unsigned int MAX_ASSET_INV_SZ = 1024;
 /** The maximum number of new addresses to accumulate before announcing. */
 static const unsigned int MAX_ADDR_TO_SEND = 1000;
 /** Maximum length of incoming protocol messages (no message over 2 MiB is currently acceptable). */
