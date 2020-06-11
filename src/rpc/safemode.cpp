1cf788c92dc3d80b9f510f00b1620e8c3d969fd9
Duality-CDOO <s.lievens@duality.solutions>
2020-06-11 10:57:37 +0200

    Amend GetArg call in safemode.cpp

--- src/rpc/safemode.cpp
+++ src/rpc/safemode.cpp
@@ -7,7 +7,7 @@
 void ObserveSafeMode()
 {
     std::string warning = GetWarnings("rpc");
-    if (warning != "" && !gArgs.GetBoolArg("-disablesafemode", DEFAULT_DISABLE_SAFEMODE)) {
+    if (warning != "" && !GetArg("-disablesafemode", DEFAULT_DISABLE_SAFEMODE)) {
         throw JSONRPCError(RPC_FORBIDDEN_BY_SAFE_MODE, std::string("Safe mode: ") + warning);
     }
 }
