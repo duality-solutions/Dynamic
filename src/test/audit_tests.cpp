181e6c12d59968edefce2dc454b6b22088abd574
dave-mann <19619811+dave-mann@users.noreply.github.com>
2020-06-15 12:27:32 -0700

    [BDAP] Add duplicate hash and signature check to audit unit test

--- src/test/audit_tests.cpp
+++ src/test/audit_tests.cpp
@@ -38,29 +38,46 @@ BOOST_AUTO_TEST_CASE(audit_test1)
         std::string strLen32 = "12345678901234567890123456789012";
         std::string strLen33 = "123456789012345678901234567890123";
         std::string strLen64 = "1234567890123456789012345678901234567890123456789012345678901234";
+
+        std::string strLen64a = "a234567890123456789012345678901234567890123456789012345678901234";
+        std::string strLen64b = "b234567890123456789012345678901234567890123456789012345678901234";
+        std::string strLen64c = "c234567890123456789012345678901234567890123456789012345678901234";
+        std::string strLen64d = "d234567890123456789012345678901234567890123456789012345678901234";
+
         std::string strLen65 = "12345678901234567890123456789012345678901234567890123456789012345";
         std::string strLen66 = "123456789012345678901234567890123456789012345678901234567890123456";
 
         CAuditData goodauditData;
         CAuditData badauditData;
         CAuditData emptyData;
+        CAuditData duplicateData;
 
-        goodauditData.vAuditData.push_back(vchFromString(strLen64));
-        goodauditData.vAuditData.push_back(vchFromString(strLen64));
-        goodauditData.vAuditData.push_back(vchFromString(strLen64));
-        goodauditData.vAuditData.push_back(vchFromString(strLen64));
+        //good unique hashes
+        goodauditData.vAuditData.push_back(vchFromString(strLen64d));
+        goodauditData.vAuditData.push_back(vchFromString(strLen64c));
+        goodauditData.vAuditData.push_back(vchFromString(strLen64b));
+        goodauditData.vAuditData.push_back(vchFromString(strLen64a));
 
-        badauditData.vAuditData.push_back(vchFromString(strLen64));
-        badauditData.vAuditData.push_back(vchFromString(strLen64));
+        //one bad hash
+        badauditData.vAuditData.push_back(vchFromString(strLen64a));
+        badauditData.vAuditData.push_back(vchFromString(strLen64b));
         badauditData.vAuditData.push_back(vchFromString(strLen65)); //the bad one
-        badauditData.vAuditData.push_back(vchFromString(strLen64));
+        badauditData.vAuditData.push_back(vchFromString(strLen64d));
 
+        //duplicate hashes
+        duplicateData.vAuditData.push_back(vchFromString(strLen64));
+        duplicateData.vAuditData.push_back(vchFromString(strLen64));
+    
         CAudit badaudit(badauditData);
         CAudit audit(goodauditData);
         CAudit emptyaudit(emptyData);
+        CAudit duplicateaudit(duplicateData);
 
         //test no hashes BAD
         BOOST_CHECK(!emptyaudit.ValidateValues(strErrorMsg) == true);
+
+        //test duplicate data BAD
+        BOOST_CHECK(!duplicateaudit.ValidateValues(strErrorMsg) == true);
 
         //test hash length BAD
         BOOST_CHECK(!badaudit.ValidateValues(strErrorMsg) == true);
@@ -109,5 +126,47 @@ BOOST_AUTO_TEST_CASE(audit_test1)
         std::cout << "Exit: audit_test1\n";
     }
 } //audit_test1
+
+
+BOOST_AUTO_TEST_CASE(audit_test2)
+{
+    {
+        //Test Signing
+        
+        std::string strErrorMsg;
+
+        std::string strLen64 = "1234567890123456789012345678901234567890123456789012345678901234";
+        std::string strLen64c = "c234567890123456789012345678901234567890123456789012345678901234";
+        std::string strLen64d = "d234567890123456789012345678901234567890123456789012345678901234";
+
+        CAuditData goodauditData;
+
+        goodauditData.vAuditData.push_back(vchFromString(strLen64d));
+        goodauditData.vAuditData.push_back(vchFromString(strLen64c));
+
+        CAudit audit(goodauditData);
+
+        //test hash data GOOD
+        BOOST_CHECK(audit.ValidateValues(strErrorMsg) == true);
+
+        //test IsSigned FALSE, not signed yet
+        BOOST_CHECK(!audit.IsSigned() == true);
+
+        CKey key;
+        key.MakeNewKey(true);
+        audit.Sign(key);
+
+        CPubKey pubKey;
+        pubKey = key.GetPubKey();
+
+        //test IsSigned TRUE
+        BOOST_CHECK(audit.IsSigned() == true);
+
+        //test valid signature
+        BOOST_CHECK(audit.CheckSignature(pubKey.Raw()));
+
+        std::cout << "Exit: audit_test2\n";
+    }
+} //audit_test2
 
 BOOST_AUTO_TEST_SUITE_END()
