8dec2af314e93b10055a95a1a25dd2b9e4462fd6
dave-mann <19619811+dave-mann@users.noreply.github.com>
2020-06-26 13:16:59 -0700

    [BDAP] Add audit add, get and verify RPC commands

--- src/rpc/audits.cpp
+++ src/rpc/audits.cpp
@@ -3,7 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "bdap/audit.h"
-//#include "bdap/auditdatadb.h"
+#include "bdap/auditdb.h"
 #include "bdap/domainentry.h"
 #include "bdap/domainentrydb.h"
 #include "bdap/fees.h"
@@ -210,10 +210,200 @@ static UniValue AddAudit(const JSONRPCRequest& request)
 #endif
 }
 
+static UniValue VerifyAudit(const JSONRPCRequest& request)
+{
+    if (request.fHelp || (request.params.size() < 2 || request.params.size() > 5))
+        throw std::runtime_error(
+            "audit verify  \"audit_hash\" \n"
+            "\nVerify audits from blockchain\n"
+            "\nArguments:\n"
+            "1. \"audit_hash         (string, required)  audit hash\n"
+            "\nResult:\n"
+            "{(json object)\n"
+            "  \"verified\"          (boolean)           Audit verified\n"
+            "  \"version\"           (string)            Audit version\n"
+            "  \"audit_count\"       (int)               Number of audit hashes stored\n"
+            "  \"timestamp\"         (int)               Time when audit was created\n"
+            "  \"owner\"             (string, optional)  BDAP account that created audit\n"
+            "  \"signed\"            (boolean)           Audit signed\n"
+            "  \"algorithm_type\"    (string, optional)  Algorithm used to create hash\n"
+            "  \"description\"       (string, optional)  Description of audit\n"
+            "  \"txid\"              (string)            Audit record transaction id\n"
+            "  \"block_time\"        (int)               Block time where audit is stored\n"
+            "  \"block_height\"      (int)               Block where audit is stored\n"
+            "}\n"
+            "\nExamples\n" +
+           HelpExampleCli("audit verify", "\"0ac6a1e929c006cc63b9220bcb40e0af1e4776c4223e6f41e0da5d16f6ea2026\" ") +
+           "\nAs a JSON-RPC call\n" + 
+           HelpExampleRpc("audit verify", "\"0ac6a1e929c006cc63b9220bcb40e0af1e4776c4223e6f41e0da5d16f6ea2026\" "));
+
+    std::vector<unsigned char> vchAudit;
+    std::vector<CAudit> vAudits;
+
+    std::string parameter1 = request.params[1].get_str();
+    vchAudit = vchFromString(parameter1);
+
+    bool readAuditState = false;
+
+    UniValue oAuditLists(UniValue::VARR);
+    UniValue oAuditList(UniValue::VOBJ);
+
+    readAuditState = pAuditDB->ReadAuditHash(vchAudit, vAudits);
+
+    if (readAuditState) {
+        for (const CAudit& audit : vAudits) {
+            UniValue oAuditList(UniValue::VOBJ);
+            oAuditList.push_back((Pair("verified", "True")));
+            BuildVerifyAuditJson(audit, oAuditList);
+            oAuditLists.push_back(oAuditList);
+        }
+    }
+    else {
+        CAudit emptyAudit;
+        oAuditList.push_back((Pair("verified", "False")));
+        BuildVerifyAuditJson(emptyAudit, oAuditList);
+        oAuditLists.push_back(oAuditList);       
+    }
+
+    return oAuditLists;
+}
+
 static UniValue GetAudits(const JSONRPCRequest& request)
 {
+    if (request.fHelp || (request.params.size() < 2 || request.params.size() > 4))
+        throw std::runtime_error(
+            "audit get  \"account | txid | audit_hash\" (start_time) (stop_time) \n"
+            "\nGets list of audits from blockchain\n"
+            "\nArguments:\n"
+            "1. \"account | txid |   (string, required)  BDAP account that created audit or Transaction ID or audit hash\n"
+            "    audit_hash\"    \n"
+            "2. \"start_time\"       (int64, optional)   Epoch start time\n"
+            "3. \"stop_time\"        (int64, optional)   Epoch stop time\n"
+            "\nResult:\n"
+            "{(json object)\n"
+            "  \"version\"           (string)            Audit version\n"
+            "  \"audit_count\"       (int)               Number of audit hashes stored\n"
+            "  \"audit_hashes\"      (string)            List of Audit hashes\n"
+            "  \"timestamp\"         (int)               Time when audit was created\n"
+            "  \"owner\"             (string, optional)  BDAP account that created audit\n"
+            "  \"signed\"            (boolean)           Audit signed\n"
+            "  \"algorithm_type\"    (string, optional)  Algorithm used to create hash\n"
+            "  \"description\"       (string, optional)  Description of audit\n"
+            "  \"txid\"              (string)            Audit record transaction id\n"
+            "  \"block_time\"        (int)               Block time where audit is stored\n"
+            "  \"block_height\"      (int)               Block where audit is stored\n"
+            "}\n"
+            "\nExamples\n" +
+           HelpExampleCli("audit get", "\"testuser\" ") +
+           "\nAs a JSON-RPC call\n" + 
+           HelpExampleRpc("audit get", "\"testuser\" "));
+
+    std::vector<unsigned char> vchOwnerFQDN;
+    std::vector<unsigned char> vchTxId;
+    std::vector<unsigned char> vchAudit;
+
+    bool searchByOwner = false;
+    bool readAuditState = false;
+    bool foundTxId = false;
+    std::string parameter1 = request.params[1].get_str();
+    std::string strOwnerFQDN = parameter1 + "@" + DEFAULT_PUBLIC_OU + "." + DEFAULT_PUBLIC_DOMAIN;
+    ToLowerCase(strOwnerFQDN);
+    vchOwnerFQDN = vchFromString(strOwnerFQDN);
+    vchTxId = vchFromString(parameter1);
+    vchAudit = vchFromString(parameter1);
+
+    int64_t epochStart;
+    int64_t epochStop;
+    int64_t retrievedTimeStamp;
+ 
+    bool startDetected = false;
+    bool stopDetected = false;
+    bool startOK = true;
+    bool stopOK = true;
+ 
+    //handle starttime [OPTIONAL]
+    if (request.params.size() > 2) {
+        if (request.params[2].get_str().size() > 0) { //only process if there's a value
+            if (!ParseInt64(request.params[2].get_str(), &epochStart))
+                throw JSONRPCError(RPC_TYPE_ERROR, "Cannot determine epoch time");
+
+            startDetected = true;
+        }
+    }
+
+    //handle stoptime [OPTIONAL]
+    if (request.params.size() > 3) {
+        if (!ParseInt64(request.params[3].get_str(), &epochStop))
+            throw JSONRPCError(RPC_TYPE_ERROR, "Cannot determine epoch time");
+
+        stopDetected = true;
+    }
+
+    // Check if name exists
+    CDomainEntry domainEntry;
+    if (GetDomainEntry(vchOwnerFQDN, domainEntry))
+        searchByOwner = true;
+
+    std::vector<CAudit> vAudits;
+    CAudit singleAudit;
+    UniValue oAuditLists(UniValue::VARR);
     UniValue oAuditList(UniValue::VOBJ);
-    return oAuditList;
+
+    //Search by owner
+    if (searchByOwner) {
+        readAuditState = pAuditDB->ReadAuditDN(vchOwnerFQDN, vAudits);
+    }
+    else { //Search by TxId
+        readAuditState = pAuditDB->ReadAuditTxId(vchTxId,singleAudit);
+        if (readAuditState) {
+            foundTxId = true;
+            BuildAuditJson(singleAudit, oAuditList);
+        }
+        if (readAuditState)
+            return oAuditList;
+    }
+
+    //If can't find owner or TxId, try Hash
+    if ((!searchByOwner) && (!foundTxId)) {
+        readAuditState = pAuditDB->ReadAuditHash(vchAudit, vAudits);
+    }
+
+    if (vAudits.size() > 0) {
+        for (const CAudit& audit : vAudits) {
+            UniValue oAuditList(UniValue::VOBJ);
+
+            if (startDetected || stopDetected)
+                retrievedTimeStamp = audit.GetTimeStamp();
+
+            if (startDetected) {
+                if (retrievedTimeStamp >= epochStart) {
+                    startOK = true;
+                }
+                else {
+                    startOK = false;
+                }
+            }
+
+            if (stopDetected) {
+                if (retrievedTimeStamp <= epochStop) {
+                    stopOK = true;
+                }
+                else {
+                    stopOK = false;
+                }
+            }
+
+            if (startOK && stopOK) {
+                BuildAuditJson(audit, oAuditList);
+                oAuditLists.push_back(oAuditList);
+            }
+
+        };
+        return oAuditLists;
+    }
+
+    //if all fails, return nothing
+   return oAuditLists;
 }
 
 UniValue audit_rpc(const JSONRPCRequest& request) 
@@ -228,13 +418,14 @@ UniValue audit_rpc(const JSONRPCRequest& request)
             "audit \"command\"...\n"
             "\nAvailable commands:\n"
             "  add                - Add new audit or list of audits\n"
-            "  get                - Get audit by hash or BDAP account\n"
+            "  get                - Get audit by hash or BDAP account or TxID\n"
+            "  verify             - Verify audit by hash\n"
             "\nExamples:\n"
             + HelpExampleCli("audit add", "\"0ac6a1e929c006cc63b9220bcb40e0af1e4776c4223e6f41e0da5d16f6ea2026\" \"testuser\" \"testdescription\" \"sha256\"") +
             "\nAs a JSON-RPC call\n"
             + HelpExampleRpc("audit add", "\"0ac6a1e929c006cc63b9220bcb40e0af1e4776c4223e6f41e0da5d16f6ea2026\" \"testuser\" \"testdescription\" \"sha256\""));
     }
-    if (strCommand == "add" || strCommand == "get") {
+    if (strCommand == "add" || strCommand == "get" || strCommand == "verify") {
         if (!sporkManager.IsSporkActive(SPORK_32_BDAP_V2))
             throw JSONRPCError(RPC_BDAP_SPORK_INACTIVE, strprintf("Can not use audit functionality until the BDAP version 2 spork is active."));
     }
@@ -243,6 +434,9 @@ UniValue audit_rpc(const JSONRPCRequest& request)
     }
     else if (strCommand == "get") {
         return GetAudits(request);
+    }
+    else if (strCommand == "verify") {
+        return VerifyAudit(request);
     } else {
         throw JSONRPCError(RPC_METHOD_NOT_FOUND, strprintf("%s is an unknown BDAP audit method command.", strCommand));
     }
@@ -252,7 +446,7 @@ UniValue audit_rpc(const JSONRPCRequest& request)
 static const CRPCCommand commands[] =
 { //  category              name                     actor (function)               okSafe argNames
   //  --------------------- ------------------------ -----------------------        ------ --------------------
-    { "bdap",               "audit",                 &audit_rpc,                    true,  {"command", "param1", "param2", "param3"} },
+    { "bdap",               "audit",                 &audit_rpc,                    true,  {"command", "param1", "param2", "param3", "param4"} },
 };
 
 void RegisterAuditRPCCommands(CRPCTable &t)
