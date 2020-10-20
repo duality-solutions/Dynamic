5f2849c6815cfa779285b4c7e010378a57402d19
SvenKercher <coin-info.net@t-online.de>
2020-10-20 18:17:31 +0200

    [DEPENDS] Upgrade expat 2.2.10

--- depends/packages/expat.mk
+++ depends/packages/expat.mk
@@ -1,8 +1,8 @@
 package=expat
-$(package)_version=2.2.5
-$(package)_download_path=https://github.com/libexpat/libexpat/releases/download/R_2_2_5/
+$(package)_version=2.2.10
+$(package)_download_path=https://github.com/libexpat/libexpat/releases/download/R_2_2_10/
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=d9dc32efba7e74f788fcc4f212a43216fc37cf5f23f4c2339664d473353aedf6
+$(package)_sha256_hash=b2c160f1b60e92da69de8e12333096aeb0c3bf692d41c60794de278af72135a5
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-static
