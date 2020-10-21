66ac525742b216aab6ea4cff04c638c37b08e615
SvenKercher <coin-info.net@t-online.de>
2020-10-21 10:04:56 +0200

    [DEPENDS] Upgrade libICE 1.0.10

--- depends/packages/libICE.mk
+++ depends/packages/libICE.mk
@@ -1,8 +1,8 @@
 package=libICE
-$(package)_version=1.0.9
+$(package)_version=1.0.10
 $(package)_download_path=http://xorg.freedesktop.org/releases/individual/lib/
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=8f7032f2c1c64352b5423f6b48a8ebdc339cc63064af34d66a6c9aa79759e202
+$(package)_sha256_hash=6f86dce12cf4bcaf5c37dddd8b1b64ed2ddf1ef7b218f22b9942595fb747c348
 $(package)_dependencies=xtrans xproto
 
 define $(package)_set_vars
