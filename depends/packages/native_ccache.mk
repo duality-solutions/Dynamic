a0bc378cae37ba3ef08627f8698f1526043489b7
SvenKercher <coin-info.net@t-online.de>
2020-10-24 11:27:18 +0200

    [DEPENDS] Upgrade ccache 3.6

--- depends/packages/native_ccache.mk
+++ depends/packages/native_ccache.mk
@@ -1,8 +1,8 @@
 package=native_ccache
-$(package)_version=3.4.2
+$(package)_version=3.6
 $(package)_download_path=https://samba.org/ftp/ccache
 $(package)_file_name=ccache-$($(package)_version).tar.bz2
-$(package)_sha256_hash=3aa5587793d4c790bd22999bfc678250c029307b3afb9d16f6f4a49c67b78fb3
+$(package)_sha256_hash=c23ecf1253e0d12c9da9dda9567a88a606d46f93d9982b8b1a423d6f238bd435
 
 define $(package)_set_vars
 $(package)_config_opts=
