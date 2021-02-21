45dad06a94384ee8f407f6f4f543ce939a712238
Kittywhiskers Van Gogh <6098974-kittywhiskers@users.noreply.gitlab.com>
2021-02-21 22:38:06 +0530

    build: add libchiabls package for DIP implementation

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,5 +1,5 @@
-packages:=boost openssl libevent
-native_packages := native_ccache 
+packages:=boost openssl libevent zeromq gmp chia_bls backtrace
+native_packages := native_ccache
 
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf zlib
