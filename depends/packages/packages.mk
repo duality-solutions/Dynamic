fe7902ce926de3253122de7dbc272f27be005695
Kittywhiskers Van Gogh <6098974-kittywhiskers@users.noreply.gitlab.com>
2021-03-09 23:20:47 +0530

    build: distinguish qt package by version number

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -4,11 +4,11 @@ native_packages := native_ccache
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf zlib
 
-qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
+qt_x86_64_linux_packages:=qt5.9 expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
 qt_i686_linux_packages:=$(qt_x86_64_linux_packages)
 
-qt_darwin_packages=qt
-qt_mingw32_packages=qt
+qt_darwin_packages=qt5.9
+qt_mingw32_packages=qt5.9
 
 wallet_packages=bdb
 
