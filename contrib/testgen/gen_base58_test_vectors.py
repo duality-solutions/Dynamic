911e4009a71df7e44e3b692b242853ee158f9410
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2021-08-07 21:42:31 +0530

    test: correct prefixes in base58 key validation tests

--- contrib/testgen/gen_base58_test_vectors.py
+++ contrib/testgen/gen_base58_test_vectors.py
@@ -15,12 +15,12 @@ import random
 from binascii import b2a_hex
 
 # key types
-PUBKEY_ADDRESS = 76
-SCRIPT_ADDRESS = 16
-PUBKEY_ADDRESS_TEST = 140
-SCRIPT_ADDRESS_TEST = 19
-PRIVKEY = 204
-PRIVKEY_TEST = 239
+PUBKEY_ADDRESS = 30
+SCRIPT_ADDRESS = 10
+PUBKEY_ADDRESS_TEST = 30
+SCRIPT_ADDRESS_TEST = 10
+PRIVKEY = 140
+PRIVKEY_TEST = 158
 
 metadata_keys = ['isPrivkey', 'isTestnet', 'addrType', 'isCompressed']
 # templates for valid sequences
